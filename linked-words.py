1925420f2327421ce5c6d38c494210161b3ed3a4
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2022-12-28 18:02:00 -0600

    Finished code

---
+++ linked-words.py
@@ -0,0 +1,9 @@
+'''
+Write a program that asks for 3 words and show them in console separated by hyphens
+'''
+
+word1 = input("Give me the first word: ")
+word2 = input("Give me the second word: ")
+word3 = input("Give me the third word: ")
+
+print(f"The words are: {word1} - {word2} - {word3}")
